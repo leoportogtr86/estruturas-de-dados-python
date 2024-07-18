83cbfcb4dd921b1b2501260f78f8621aa3c46605
Leo Porto <leonardoreisporto@gmail.com>
2024-07-18 10:12:54 -0300

    Exercícios

---
+++ exercicios/respostas_1_arrays/ex5.py
@@ -0,0 +1,5 @@
+def inverso(arr):
+    return arr[::-1]
+
+
+print(inverso([10, 20, 30, 40]))
---
+++ exercicios/respostas_1_arrays/ex6.py
@@ -0,0 +1,6 @@
+# Escreva uma função que receba um array de números inteiros e retorne a média dos elementos.
+def media(arr):
+    return sum(arr) / len(arr)
+
+
+print(media([10, 8, 7, 9]))
