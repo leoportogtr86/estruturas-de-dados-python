fd66dd89b600fa4a102aa8e1ff39db6409bcb81f
Leo Porto <leonardoreisporto@gmail.com>
2024-07-18 15:04:54 -0300

    Estruturas de Dados Básicas: Listas Ligadas

---
+++ exercicios/respostas_2_strings/ex5.py
@@ -0,0 +1,5 @@
+def substituir_palavra(frase, palavra_origem, palavra_destino):
+    return frase.replace(palavra_origem, palavra_destino)
+
+
+print(substituir_palavra("Olá, Leo", "Leo", "Leonardo"))
